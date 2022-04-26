87f60e84b11581f4798731967fa056ed2c635eff
tomer shimshi <tomer.shimshi@brightmachines.com>
2022-04-26 14:18:14 +0300

    fixed the problem for Q1

--- HW3_203200480.py
+++ HW3_203200480.py
@@ -1,15 +1,31 @@
 import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics.pairwise import rbf_kernel
+from sklearn.gaussian_process.kernels import RBF
 from sklearn.metrics.pairwise import polynomial_kernel
+import scipy
+
+
+def RBF (x1,x2,sigma):
+    M =len(x1)
+    N=len(x2)
+    k= np.zeros((M,N))
+    for i in range(M):
+        for j in range(N):
+            temp1 =x1[i]
+            temp=  scipy.spatial.distance.euclidean(x1[i],x2[j])
+            k[i][j]=np.exp(-(temp/2*sigma**2))
+    return k
+
+
 #contants to change
-T_rbf=100
+T_rbf=50
 T_poly = 100
-sigma= 0.4 # best based on script
+sigma= 4 # best based on script
 gamma = 0.5*sigma**2
 q=0.1# best by tester
 # add a var to decide wether to run the rbf or the poly kernal
-run = 'poly'
+run = 'rbf'
 # get the train data an plot it 
 train = np.loadtxt("data/train.csv", delimiter=',')
 train=np.asarray(train)
@@ -32,7 +48,8 @@ alpha_rbf = np.zeros(m)
 alpha_poly = np.zeros(m)
 # create the K matrix
 
-K_rbf = rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
+K_rbf = RBF(train[:,0:2],train[:,0:2],sigma)#rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
+#K_rbf2 = RBF(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
 k_poly = polynomial_kernel(train[:,0:2],train[:,0:2],coef0=q)
 
 
@@ -60,7 +77,13 @@ if run == 'rbf':
     for t in range(T_rbf):
         error_count_rbf =0
         for j in range(m):
-            y_hat= np.sign(np.sum(alpha_rbf*K_rbf[j]))
+            #y_hat= np.sign(np.sum(alpha_rbf*K_rbf[:,j]))
+            y_hat= np.sign(np.dot(alpha_rbf,K_rbf[:,j]))
+            #temp =np.sum(alpha_rbf*K_rbf[:,j])
+            #if temp >0:
+            #    y_hat =1
+            #else:
+            #    y_hat =0
             alpha_rbf[j]+=0.5*(train[j,2]-y_hat)
             if y_hat != train[j,2]:
                 error_count_rbf +=1
@@ -72,11 +95,17 @@ if run == 'rbf':
                     plt.scatter(train[j, 0:1],train[j, 1:2],color='orange') 
                 elif y_hat == 1:
                     plt.scatter(train[j, 0:1],train[j, 1:2],color='blue') 
+        print('after iter num {} we got {} errors in rbf'.format(t,error_count_rbf)) 
+        if error_count_rbf == 0:
+            break
+            #e=2
 
-        print('after iter num {} we got {} errors in rbf'.format(t,error_count_rbf))  
+         
     #start with rbf
+    K_rbf= RBF(train[:,0:2],test[:,0:2],sigma)#rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
     for j in range(n):
-        y_hat= np.sign(np.sum(alpha_rbf*K_rbf[j]))
+        #y_hat= np.sign(np.sum(alpha_rbf*K_rbf[j]))
+        y_hat= np.sign(np.dot(alpha_rbf,K_rbf[:,j]))
         shuff = test[j,2]
         if y_hat != test[j,2]:
             error_count_rbf +=1
@@ -114,6 +143,7 @@ else:
         print('after iter num {} we got {} errors in poly'.format(t,error_count))  
 
     #now poly
+    k_poly = polynomial_kernel(train[:,0:2],test[:,0:2],coef0=q)
     error_count_poly =0
     for j in range(n):
         y_hat= np.sign(np.sum(alpha_poly*k_poly[j]))
--- tester.py
+++ tester.py
@@ -2,12 +2,19 @@ import numpy as np
 from matplotlib import pyplot as plt
 from sklearn.metrics.pairwise import rbf_kernel
 from sklearn.metrics.pairwise import polynomial_kernel
+import scipy
 
-
-T_rbf=100
+T_rbf=50
 T_poly = 100
-sigma_max= 100
-
+sigma_max= 50
+def RBF (x1,x2,sigma):
+    k= np.zeros((147,147))
+    for i in range(len(x1)):
+        for j in range(len(x2)):
+            temp1 =x1[i]
+            temp=  scipy.spatial.distance.euclidean(x1[i],x2[j])
+            k[i][j]=np.exp(-(temp/(2*sigma**2)))
+    return k
 q=0.2
 # add a var to decide wether to run the rbf or the poly kernal
 
@@ -18,14 +25,15 @@ best_sigma =4
 min_errors = 80
 for sigma in range(1,sigma_max):
     sigma = float(sigma/10.0)
-    gamma = 0.5*sigma**2
+    #gamma = 0.5*sigma**2
     #plt.show()
     m= len(train)
 
     alpha_rbf = np.zeros(m)
     # create the K matrix
 
-    K_rbf = rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
+    #K_rbf = rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
+    K_rbf = RBF(train[:,0:2],train[:,0:2],sigma)#rbf_kernel(train[:,0:2],train[:,0:2],gamma)#np.zeros((m,m))
 
 
     # Test the 2 alogorithems
@@ -47,14 +55,14 @@ for sigma in range(1,sigma_max):
     for t in range(T_rbf):
         error_count_rbf =0
         for j in range(m):
-            y_hat= np.sign(np.sum(alpha_rbf*K_rbf[j]))
+            y_hat= np.sign(np.dot(alpha_rbf,K_rbf[:,j]))
             alpha_rbf[j]+=0.5*(train[j,2]-y_hat)
             if y_hat != train[j,2]:
                 error_count_rbf +=1
  
     #start with rbf
     for j in range(n):
-        y_hat= np.sign(np.sum(alpha_rbf*K_rbf[j]))
+        y_hat= np.sign(np.dot(alpha_rbf,K_rbf[:,j]))
         shuff = test[j,2]
         if y_hat != test[j,2]:
             error_count_rbf +=1
